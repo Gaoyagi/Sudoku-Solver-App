3f85233968a5ea2fa1815752c3eb1007a9addcbb
George Aoyagi <aoyagigeorge@gmail.com>
2020-01-10 00:12:31 -0800

    Initial Commit

---
+++ SudokuSolverApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SudokuSolverApp
+//
+//  Created by George Aoyagi on 1/10/20.
+//  Copyright © 2020 George Aoyagi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
